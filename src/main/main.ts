fc24a0f30e95392f0987457a2d87f83af453a3e1
Vinicius Boscoa <virb30@gmail.com>
2026-04-23 23:19:29 -0300

    docs: plans contract first ipc refactor

--- src/main/main.ts
+++ src/main/main.ts
@@ -2,7 +2,7 @@ import { app, BrowserWindow, ipcMain } from 'electron';
 import { createMainLifecycle } from './infrastructure/composition/create-main-lifecycle';
 import { createAndInitializeDatabase } from './database/database';
 import { WindowManager } from './window-manager';
-import { AppCradle, container, registerDependencies } from './infrastructure/container';
+import { container, registerDependencies } from './infrastructure/container';
 
 const windowManager = new WindowManager();
 
